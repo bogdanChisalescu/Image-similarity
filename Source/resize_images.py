d4d6bfc926007ef0717041422735598f164a818f
IulianChivu <iulianchivu98@yahoo.com>
2021-01-05 18:18:29 +0200

    Added a script that resizes the images from the dataset

---
+++ Source/resize_images.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+
+#getting the current working directory and 
+#changing it to the images folder
+cwd = os.path.abspath(os.getcwd())
+cwd = cwd.replace("Source", "Images")
+
+#list containing all the names of the images
+images = os.listdir(cwd)
+
+#resize the data set
+for i in range(len(images)):
+    img = Image.open(cwd + "\\" +images[i])
+    img = img.resize((384, 256))
+    img.save(cwd + "\\" + images[i])
